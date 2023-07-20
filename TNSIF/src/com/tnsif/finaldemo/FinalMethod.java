ad2841131f11fda30d2f9346022aef7564e4dce4
TUSHAR TELKAR <TUSHAR TELKAR@DESKTOP-RVRVF0E>
2023-07-20 12:08:39 +0530

    final method

---
+++ TNSIF/src/com/tnsif/finaldemo/FinalMethod.java
@@ -0,0 +1,11 @@
+package com.tnsif.finaldemo;
+
+public class FinalMethod {
+
+	final int a=10;
+	
+	final void show()
+	{
+		System.out.println(a);
+	}
+}
---
+++ TNSIF/src/com/tnsif/finaldemo/FinalMethodDemo.java
@@ -0,0 +1,19 @@
+package com.tnsif.finaldemo;
+
+public class FinalMethodDemo extends FinalMethod{
+
+	/*
+	 * public void show() //cannot override final methods {
+	 * 
+	 * }
+	 */
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		FinalMethod fm=new FinalMethod();
+		fm.show();
+	}
+
+}
