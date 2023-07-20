28d6fbf62e96c31f89ad3cbd47737de43afe4453
TUSHAR TELKAR <TUSHAR TELKAR@DESKTOP-RVRVF0E>
2023-07-20 12:10:09 +0530

    Polymorphism programs

---
+++ Polymorphism/src/com/tnsif/compiletime/OverloadingDemo.java
@@ -0,0 +1,18 @@
+package com.tnsif.compiletime;
+
+public class OverloadingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Point p=new Point();
+		System.out.println(p);
+		
+		Point p1=new Point(2);
+		System.out.println(p1);
+		
+		Point p2=new Point(3,4);
+		System.out.println(p2);
+	}
+
+}
